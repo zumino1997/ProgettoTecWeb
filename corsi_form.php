5e5a887b6954d07239d7f799dd101227333605bf
Simone Franconetti <simone.franconetti@gmail.com>
2019-06-27 15:43:11 +0200

    sistemata la icona del menù ad hamburger + i meta tag

--- corsi_form.php
+++ corsi_form.php
@@ -15,25 +15,23 @@ $_SESSION ['paginaCorr']="";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it"><!-- il comando html permette di importare un namespace, contenente tutto l'insieme dei tag utilizzabili -->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
 
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <!--metatag che permette di dire che cosa ci sara' all'interno della pagina. Consiglio e' sempre quello di utilizzare la codifica utf-8 perche' e' univoco per la codifica degli accenti-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Energya Fitness Club</title>
 <meta name="title" content="Energya Fitness Club"/>
-<meta name="description" content=""/>
+<meta name="description" content="Energya - un luogo per tutti quelli che amano la palestra e vogliono sentirsi come in famiglia!"/>
 <meta name="keywords" content="Energya, fitness, palestra, sport"/>
 <meta name="language" content="italian it"/>
-<meta name="author" content="Franconetti Simone"/>
+<meta name="author" content="Franconetti Simone, Infantino Matteo, Marcon Luca"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
+
 <link rel="stylesheet" type="text/css" href="CSS/css_index.css" media="handheld, screen"/>
-<link rel="stylesheet" type="text/css" href="CSS/css_index_small_1200px.css" media="handheld, screen and (max-width:1200px),only screen and (max-device-width:1200px)"/>
 <link rel="stylesheet" type="text/css" href="CSS/css_index_small_768px.css" media="handheld, screen and (max-width:768px),only screen and (max-device-width:720px)"/>
 <link rel="stylesheet" type="text/css" href="CSS/css_index_small_480px.css" media="handheld, screen and (max-width:480px),only screen and (max-device-width:480px)"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.8.2/css/all.css'>
 <link rel="stylesheet" type="text/css" href="CSS/print.css" media="print"/>
 
 
@@ -46,7 +44,7 @@ $_SESSION ['paginaCorr']="";
 <body>
 	<div id="nav">
 	  <div id="logo"><img src="IMG/logo1.png" alt="Logo Energya"/></div>
-    <button id="menuIcon" onclick="menuHamburger()"><i class='fas fa-bars'></i></button>
+    <button id="menuIcon" onclick="menuHamburger()" aria-label="menu icon"><img src="IMG/hamburger_icon.png" alt="icona menu"/></button>
 	  <ul class="menuItems" id="menuu" >
       <li><a href="index.php" xml:lang="en">Home</a></li>
 	    <li><a href="corsi.php">Corsi</a></li>
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it"><!-- il comando html permette di importare un namespace, contenente tutto l'insieme dei tag utilizzabili -->
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Energya Fitness Club</title>
+<meta name="title" content="Energya Fitness Club"/>
+<meta name="description" content="Energya - un luogo per tutti quelli che amano la palestra e vogliono sentirsi come in famiglia!"/>
+<meta name="keywords" content="Energya, fitness, palestra, sport"/>
+<meta name="language" content="italian it"/>
+<meta name="author" content="Franconetti Simone, Infantino Matteo, Marcon Luca"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 
 <?php
 if (session_status() == PHP_SESSION_NONE) {
@@ -10,22 +22,12 @@ $_SESSION ['paginaCorr']="";
 
 ?>
 
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <!--metatag che permette di dire che cosa ci sara' all'interno della pagina. Consiglio e' sempre quello di utilizzare la codifica utf-8 perche' e' univoco per la codifica degli accenti-->
-<title>Energya Fitness Club</title>
-<meta name="title" content="Energya Fitness Club"/>
-<meta name="description" content="Energya Fitness Club"/>
-<meta name="keywords" content="Energya, fitness, palestra, sport"/>
-<meta name="language" content="italian it"/>
-<meta name="author" content="Franconetti Simone"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
 
 
 <link rel="stylesheet" type="text/css" href="CSS/css_index.css" media="handheld, screen"/>
 <link rel="stylesheet" type="text/css" href="CSS/css_index_small_768px.css" media="handheld, screen and (max-width:768px),only screen and (max-device-width:768px)"/>
 <link rel="stylesheet" type="text/css" href="CSS/css_index_small_480px.css" media="handheld, screen and (max-width:480px),only screen and (max-device-width:480px)"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css'/>
 <link rel="stylesheet" type="text/css" href="CSS/print.css" media="print"/>
 
 
@@ -38,7 +40,7 @@ $_SESSION ['paginaCorr']="";
 
 	<div id="nav">
 	  <div id="logo"><img src="IMG/logo1.png" alt="Logo Energya"/></div>
-	  <button aria-label="menu button" id="menuIcon" onclick="menuHamburger()"><i class='fas fa-bars'></i></button>
+    <button id="menuIcon" onclick="menuHamburger()" aria-label="menu icon"><img src="IMG/hamburger_icon.png" alt="menu icon"/></button>
 	  <ul class="menuItems" id="menuu" >
 	    <li id="currentLink" xml:lang="en">Home</li>
 	    <li><a href="corsi.php">Corsi</a></li>
